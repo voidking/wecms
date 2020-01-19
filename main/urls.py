635047ee6a89360f3484cd265e8628ef3857c339
voidking <voidking@qq.com>
2020-01-19 21:39:15 +0800

    用户发送任意信息，都返回Today is wonderful day!

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from . import views_wechat
+from werobot.contrib.django import make_view
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('index/',views.index, name='index'),
+    path('',views.index),
+    path('index/',views.index),
+    path('wechat/',make_view(views_wechat.robot)),
 ]
