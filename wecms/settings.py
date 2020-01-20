df54707718d3a8b8df1c181d2010718c3df3bfb1
voidking <voidking@qq.com>
2020-01-20 20:39:53 +0800

    修改配置，添加ALLOWED_HOSTS

--- wecms/settings.py
+++ wecms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v7i6f57eb-#x76%k_hr(4*juuu-rh$h_+@e0gr8#w!p*uz#fag'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
