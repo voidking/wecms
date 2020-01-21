32647df0df02f35af0b7141c1fb201bb0590f24d
haojin <haojin@vipkid.com.cn>
2020-01-21 10:53:53 +0800

    根据不同的消息类型，返回不同的信息

--- main/views_wechat.py
+++ main/views_wechat.py
@@ -4,3 +4,27 @@ robot = werobot.WeRoBot(token='vkwechat')
 @robot.handler
 def index(message):
     return 'Today is wonderful day!'
+
+@robot.text
+def text(message):
+    return '您发送了文本消息，内容为：' + message.content
+
+@robot.image
+def image(message):
+    return '您发送了图片消息，图片为：' + message.img
+
+@robot.link
+def link(message):
+    return '您发送了链接消息，链接为：' + message.url
+
+@robot.location
+def location(message):
+    return '您发送了位置消息，位置为：' + message.label
+
+@robot.voice
+def voice(message):
+    return '您发送了声音消息，media_id为：' + message.media_id
+
+@robot.video
+def video(message):
+    return '您发送了视频消息，media_id为：' + message.media_id
